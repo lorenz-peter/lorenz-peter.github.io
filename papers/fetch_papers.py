f6545475377d8ee399d9525cd0469867ae79b751
jS5t3r <jS5t3r@users.noreply.github.com>
2024-10-13 13:51:23 +0000

    Deploying to gh-pages from @ lorenz-peter/lorenz-peter.github.io@12e9b26691c847355cfcd3ee3c22ffbaf348b017 🚀

--- papers/fetch_papers.py
+++ papers/fetch_papers.py
@@ -1,7 +1,13 @@
-import arxiv
 import json
+import arxiv # https://colab.research.google.com/github/EPS-Libraries-Berkeley/volt/blob/main/Search/arxiv_api.ipynb
 
-# Define your search parameters
+
+# Construct the default API client.
+# client = Client()
+# https://lukasschwab.me/arxiv.py/arxiv.html
+# Perform the search using arxiv.Search
+
+
 search = arxiv.Search(
     query="machine learning",  # Modify this to your desired keywords
     max_results=10,
@@ -10,9 +16,10 @@ search = arxiv.Search(
 
 papers_data = []
 
+# Iterate over the results from search
 for result in search.results():
     papers_data.append({'id': result.entry_id, 'title': result.title})
 
 # Save to JSON file
 with open('papers.json', 'w') as f:
-    json.dump(papers_data, f)
+    json.dump(papers_data, f, indent=4)
