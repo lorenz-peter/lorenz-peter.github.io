ba49f603cd5f2cbf99279f31b385422df8653136
peter <peter.lorenz@ntu.edu.sg>
2024-10-18 14:06:24 +0800

    update papers

--- papers/fetch_papers.py
+++ papers/fetch_papers.py
@@ -1,3 +1,4 @@
+import os
 import json
 import arxiv # https://colab.research.google.com/github/EPS-Libraries-Berkeley/volt/blob/main/Search/arxiv_api.ipynb
 
@@ -9,17 +10,20 @@ import arxiv # https://colab.research.google.com/github/EPS-Libraries-Berkeley/v
 
 
 search = arxiv.Search(
-    query="machine learning",  # Modify this to your desired keywords
-    max_results=10,
-    sort_by=arxiv.SortCriterion.SubmittedDate
+    query="(cat:cs.CR OR cat:cs.LG OR cat:cs.AI) AND (model stealing OR model extraction or high-fidelity)",
+    max_results=100,
+    sort_by=arxiv.SortCriterion.SubmittedDate,
+    sort_order=arxiv.SortOrder.Descending
 )
 
 papers_data = []
 
 # Iterate over the results from search
 for result in search.results():
-    papers_data.append({'id': result.entry_id, 'title': result.title})
+    # breakpoint()
+    authors = [author.name for author in result.authors]
+    papers_data.append({'id': result.entry_id, 'title': result.title, 'authors': ', '.join(authors)})
 
 # Save to JSON file
-with open('papers.json', 'w') as f:
+with open(os.path.join('assets/json', 'model_stealing_papers.json'), 'w') as f:
     json.dump(papers_data, f, indent=4)
